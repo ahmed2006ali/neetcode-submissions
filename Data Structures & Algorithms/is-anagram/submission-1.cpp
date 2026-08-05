106cb650d5284f33c99407a7e6d0eab1bbb155e9
ahmed2006ali <aadevswe@gmail.com>
2026-08-05 20:55:59 +0300

    Update: is-anagram - submission-1

--- Data Structures & Algorithms/is-anagram/submission-1.cpp
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
-        if (s.length() != t.length()) return false; 
-        
-        unordered_map<char, int> s_Map;
-        unordered_map<char, int> t_Map;
-        
-        for(char c : s) s_Map[c]++;
-        for(char c : t) t_Map[c]++;
-        
-        return s_Map == t_Map;
+        unordered_map<char,int> um_s;
+        unordered_map<char,int> um_t;
+        for(char c : s){
+            um_s[c]++;
+        }
+        for(char c : t){
+            um_t[c]++;
+        }
+        return um_s == um_t ; 
     }
 };
