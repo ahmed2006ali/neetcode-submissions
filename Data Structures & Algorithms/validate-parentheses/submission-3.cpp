dd29167d8c6b7cb31eb158cff4e6255befdf401b
ahmed2006ali <aadevswe@gmail.com>
2026-08-05 21:03:58 +0300

    Update: validate-parentheses - submission-3

--- Data Structures & Algorithms/validate-parentheses/submission-3.cpp
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -1,33 +1,26 @@
-#include<assert.h>
 class Solution {
 public:
     bool isValid(string s) {
+        unordered_map<char,char> um;
         stack<char> st;
-        
-        assert(s.length()!=0);
-        
-        int size = s.length();
-        
-        for(int i=0;i<size;i++){
-            if(s[i] =='(' | s[i]=='[' | s[i]=='{'){
-                st.push(s[i]);
+        um ={
+            {'[',']'},
+            {'(',')'},
+            {'{','}'}
+        };
+
+        for(auto c : s){
+
+            if(um.contains(c)) st.push(c); //is opening bracket always push
+
+            else if(st.empty() && !(um.contains(c))){
+                return false; //is closing bracket and the stack is empty
             }
-            else if(st.empty() &&( s[i] == ')' | s[i] == ']' | s[i] == '}')){
-                return false;
-            }
-            else if(s[i]==')' && st.top()=='('){
-                st.pop();
-            }
-            else if(s[i]==']' && st.top()=='['){
-                st.pop();
-            }
-            else if(s[i]=='}' && st.top()=='{'){
-                st.pop();
-            }
-            else{
-                st.push(s[i]);
-            }
+
+            else if (um[st.top()] == c) st.pop();
+            else return false;
+
         }
-        return st.empty();
+            return st.empty();
     }
 };
